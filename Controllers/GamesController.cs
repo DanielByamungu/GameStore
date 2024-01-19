af78a0886381824df99e8b4177cbfe6834be6d42
DanielByamungu <56130261+DanielByamungu@users.noreply.github.com>
2024-01-19 13:26:29 -0500

    last commit on the game store webapp, all features tested working well

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.Models;
+using EntityState = System.Data.Entity.EntityState;
+
+namespace GameStore.Controllers
+{
+    public class GamesController : Controller
+    {
+        private CVGS_Tables db = new CVGS_Tables();
+        /*
+        private System.Text.RegularExpressions.Regex NotYoutubeRegex =
+            new System.Text.RegularExpressions.Regex (Validators.MyRegex.NotYoutubeRegex);
+        private string youtubeViewLink = "https://www.youtube.com/watch?v=";
+        */
+        public GamesController() { }
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var games = db.Games.Include(g => g.EsrbRating)
+                                .Include(g => g.GameCategory)
+                                .Include(g => g.GamePerspective)
+                                .Include(g => g.GameStatu)
+                                .Include(g => g.GameSubCategory);
+            return View(games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            if (game.FrenchVersion && game.FrenchTrailer != null)
+            {
+                game.FrenchTrailer = Regex.Replace(game.FrenchTrailer, Validators.MyRegex.NotYoutubeRegex, "");
+            }
+            game.EnglishTrailer = Regex.Replace(game.EnglishTrailer, Validators.MyRegex.NotYoutubeRegex, "");
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.EsrbRatingCode = new SelectList(db.EsrbRatings, "Code", "EnglishRating");
+            ViewBag.GameCategoryId = new SelectList(db.GameCategories, "Id", "EnglishCategory");
+            ViewBag.GamePerspectiveCode = new SelectList(db.GamePerspectives, "Code", "EnglishPerspectiveName");
+            ViewBag.GameStatusCode = new SelectList(db.GameStatus, "Code", "EnglishCategory");
+            ViewBag.GameSubCategoryId = new SelectList(db.GameSubCategories, "Id", "EnglishCategory");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Guid,GameStatusCode,GameCategoryId,GameSubCategoryId,EsrbRatingCode," +
+            "EnglishName,FrenchName,FrenchVersion,EnglishPlayerCount,FrenchPlayerCount,GamePerspectiveCode," +
+            "EnglishTrailer,FrenchTrailer,EnglishDescription,FrenchDescription,EnglishDetail,FrenchDetail,UserName")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (!game.FrenchVersion)
+                {
+                    game.FrenchName = "";
+                    game.FrenchPlayerCount = "0";
+                    game.FrenchTrailer = "";
+                    game.FrenchDescription = "";
+                    game.FrenchDetail = "";
+                }
+                game.Guid = Guid.NewGuid();
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EsrbRatingCode = new SelectList(db.EsrbRatings, "Code", "EnglishRating", game.EsrbRatingCode);
+            ViewBag.GameCategoryId = new SelectList(db.GameCategories, "Id", "EnglishCategory", game.GameCategoryId);
+            ViewBag.GamePerspectiveCode = new SelectList(db.GamePerspectives, "Code", "EnglishPerspectiveName", game.GamePerspectiveCode);
+            ViewBag.GameStatusCode = new SelectList(db.GameStatus, "Code", "EnglishCategory", game.GameStatusCode);
+            ViewBag.GameSubCategoryId = new SelectList(db.GameSubCategories, "Id", "EnglishCategory", game.GameSubCategoryId);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            if (game.FrenchVersion && game.FrenchTrailer != null)
+            {
+                game.FrenchTrailer = Regex.Replace(game.FrenchTrailer, Validators.MyRegex.NotYoutubeRegex, "https://www.youtube.com/watch?v=");
+            }
+            game.EnglishTrailer = Regex.Replace(game.EnglishTrailer, Validators.MyRegex.NotYoutubeRegex, "https://www.youtube.com/watch?v=");
+            ViewBag.EsrbRatingCode = new SelectList(db.EsrbRatings, "Code", "EnglishRating", game.EsrbRatingCode);
+            ViewBag.GameCategoryId = new SelectList(db.GameCategories, "Id", "EnglishCategory", game.GameCategoryId);
+            ViewBag.GamePerspectiveCode = new SelectList(db.GamePerspectives, "Code", "EnglishPerspectiveName", game.GamePerspectiveCode);
+            ViewBag.GameStatusCode = new SelectList(db.GameStatus, "Code", "EnglishCategory", game.GameStatusCode);
+            ViewBag.GameSubCategoryId = new SelectList(db.GameSubCategories, "Id", "EnglishCategory", game.GameSubCategoryId);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Guid,GameStatusCode,GameCategoryId,GameSubCategoryId,EsrbRatingCode,EnglishName,FrenchName,FrenchVersion,EnglishPlayerCount,FrenchPlayerCount,GamePerspectiveCode,EnglishTrailer,FrenchTrailer,EnglishDescription,FrenchDescription,EnglishDetail,FrenchDetail,UserName")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!game.FrenchVersion)
+                {
+                    game.FrenchName = "";
+                    game.FrenchPlayerCount = "0";
+                    game.FrenchTrailer = "";
+                    game.FrenchDescription = "";
+                    game.FrenchDetail = "";
+                }
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EsrbRatingCode = new SelectList(db.EsrbRatings, "Code", "EnglishRating", game.EsrbRatingCode);
+            ViewBag.GameCategoryId = new SelectList(db.GameCategories, "Id", "EnglishCategory", game.GameCategoryId);
+            ViewBag.GamePerspectiveCode = new SelectList(db.GamePerspectives, "Code", "EnglishPerspectiveName", game.GamePerspectiveCode);
+            ViewBag.GameStatusCode = new SelectList(db.GameStatus, "Code", "EnglishCategory", game.GameStatusCode);
+            ViewBag.GameSubCategoryId = new SelectList(db.GameSubCategories, "Id", "EnglishCategory", game.GameSubCategoryId);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GameStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        CVGS_Tables db = new CVGS_Tables();
+        public ActionResult Index(string search)
+        {
+            var games = from m in db.Games
+                         select m;
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                games = games.Where(s => s.EnglishName.Contains(search));
+            }
+
+            return View(games.ToList());
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Log in page";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Signup(Person U)
+        {
+            if(ModelState.IsValid)
+            {
+                using (CVGS_Tables db = new CVGS_Tables())
+                {
+                    U.Id = 10;
+                    var check = db.People.FirstOrDefault(s => s.Email == U.Email);
+                    if (check == null)
+                    {
+                        U.ProvinceCode = "ON";
+                        U.CountryCode = "CAN";
+                        
+                        db.Configuration.ValidateOnSaveEnabled = false;
+                        db.People.Add(U);
+                        db.SaveChanges();
+
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.error = "Email already exists";
+                        return View();
+                    }
+                }
+            }
+            return View();
+        }
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+        public ActionResult Account()
+        {
+            ViewBag.Message = "User's profile page";
+            if (Session["userId"] == null)
+            {
+                TempData["Message"] = "Login to view your account info";
+                Response.Redirect("~/Home/Login");
+            }
+
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Page with the list of events";
+
+            return View();
+        }
+
+        public ActionResult AdministrationPortal()
+        {
+            ViewBag.Message = "Administration Portal";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Person personModel)
+        {
+            using (CVGS_Tables db = new CVGS_Tables())
+            {
+                var userDetails = db.People.ToList().Where(x => (x.UserName == personModel.UserName && 
+                x.Password == personModel.Password)).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    personModel.LoginErrorMessage = "Wrong Username or Password";
+                    return View("Login", personModel);
+                }
+                else
+                {
+                    Response.Write("<script language=javascript>alert('You are now logged in')</script>");
+                    Session["userId"] = userDetails.Id;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
